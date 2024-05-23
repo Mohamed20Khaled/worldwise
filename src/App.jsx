a083ab64bacf2e9b64666da64cd951da51808b77
Mohamed20Khaled <mk740015@gmail.com>
2024-05-23 14:08:59 +0300

    starter file

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/" element={<Home />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
